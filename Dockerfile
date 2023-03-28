7215188c3bcc2ab62468aaf79a21882b74b607ba
pitekusu <pitekusu@gmail.com>
2023-03-28 21:36:54 +0900

    一旦ね

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,13 @@ FROM python:3.11.2-slim-bullseye
 WORKDIR /app
 COPY . /app
 
-RUN apt-get update && apt-get install -y ffmpeg
+#RUN apt-get update && apt-get install -y ffmpeg
 
+#ECS環境ではdotenv不要
 RUN python -m pip install \
-    discord.py[voice]
+    discord.py[voice] \
+    python-dotenv
+
+
 
 CMD ["python3.11", "kancolle-bot.py"]
--- kancolle-bot.py
+++ kancolle-bot.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
 import discord
 from discord import app_commands
 
+# ECS環境ではdotenv不要
+load_dotenv()
+
 TOKEN = os.environ['TOKEN']
+MY_GUILD_ID = os.environ['MY_GUILD_ID']
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -14,7 +19,7 @@ tree = app_commands.CommandTree(client)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await tree.sync()
+    await tree.sync(guild=discord.Object(id=MY_GUILD_ID))
 
 
 @client.event
@@ -22,19 +27,21 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('hello'):
+    if message.content.startswith('bukki'):
         await message.channel.send('隠しブッキー!')
-        
+
     if message.content == "!join":
         if message.author.voice is None:
-            await message.channel.send("司令官はボイスチャンネルに接続していないようです。")
+            await message.channel.send('司令官はボイスチャンネルに接続していないようです。')
             return
     # ボイスチャンネルに接続する
-    await message.author.voice.channel.connect()
-    await message.channel.send("接続しました！")
+        await message.author.voice.channel.connect()
+        await message.channel.send('接続しました！')
 
-@tree.command(name="test",description="テストコマンドです。")
-async def test_command(interaction: discord.Interaction,text:str="てすと"):#デフォルト値を指定
-    await interaction.response.send_message(text,ephemeral=True)
+
+@tree.command(name='join', description='ブッキーがボイスチャンネルに来ます', guild=discord.Object(id=MY_GUILD_ID))
+async def join_command(interaction: discord.Interaction, channel_name: discord.VoiceChannel):
+    await interaction.response.send_message('チャンネルに参加します！')
+    await channel_name.connect()
 
 client.run(TOKEN)
