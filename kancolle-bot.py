e3f19d82595007f868cb642395cabfb071d57fc2
pitekusu <pitekusu@gmail.com>
2023-05-05 20:18:23 +0900

    2人以上の艦むすに対応する#5

--- kancolle-bot.py
+++ kancolle-bot.py
@@ -129,7 +129,7 @@ async def loop():
     now = datetime.now(JST).strftime('%H:%M:%S')
     if now in dateTimeList:
         await play_sound()
-    elif now == '20:10:00':
+    elif now == '23:45:00':
         kanmusu_count = len(get_all_kanmusu())
         random_num = random.randint(0, kanmusu_count - 1)
         global Kanmusu
